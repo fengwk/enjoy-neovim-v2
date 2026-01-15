02a2535249ac11360283680b3eb18272fcf8e494
fengweikang@corp.netease.com <fengweikang@corp.netease.com>
2026-01-15 22:55:56 +0800

    fix

--- lua/fengwk/plugins/markdown.lua
+++ lua/fengwk/plugins/markdown.lua
@@ -9,7 +9,7 @@ return {
     init = function()
       vim.g.mkdp_auto_close = 0
       -- vim.g.mkdp_open_to_the_world = 1 -- bind 0.0.0.0
-      vim.g.mkdp_port = 38888
+      vim.g.mkdp_port = "" -- 自动端口防止多工程打开的时候端口冲突
       vim.g.mkdp_open_ip = "127.0.0.1" -- 公司的安全软件会禁用 localhost
       vim.g.mkdp_theme = globals.theme.bg
       vim.g.mkdp_filetypes = globals.markdown_filetypes
